2a0c5fa15f40e8fe8647d787dbee49773708fcbf
Pradyumna Bhattarai <connectwithpradyumna@gmail.com>
2023-09-06 10:01:20 -0700

    Added Course Module

---
+++ Courses.java
@@ -0,0 +1,11 @@
+public class Courses {
+    String course_name;
+
+    Courses(String course_name) {
+        this.course_name = course_name;
+    }
+
+    void printCourseName() {
+        System.out.println("The Course Name is :" + this.course_name);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
-public class Main{
+public class Main {
 
+    public static void main(String args[]) {
 
-public static void main (String args[]){
+        System.out.println("Hello From Lappy");
+        System.out.println("Changed From Local Repo.");
+        System.out.println("Aka Jenkins Worked");
 
-System.out.println("Hello From Lappy");
-System.out.println("Changed From Local Repo.");
-System.out.println("Aka Jenkins Worked");
+        Student s = new Student("Pradyumna", "101JenkinsTrainee");
+        s.printDetails();
 
-Student s = new Student("Pradyumna","101JenkinsTrainee");
-s.printDetails();
+        Courses c = new Courses("CSIT");
+        c.printCourseName();
 
-}
+    }
 }
