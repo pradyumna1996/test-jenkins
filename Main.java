202bff0a9afba678e7cef2e307b86cbe2db8233c
Pradyumna Bhattarai <connectwithpradyumna@gmail.com>
2023-09-06 09:40:11 -0700

    Added Some Lines To Test Jenkins Work

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main{
 public static void main (String args[]){
 
 System.out.println("Hello From Lappy");
+System.out.println("Changed From Local Repo.");
+System.out.println("Aka Jenkins Worked");
+
 }
 }
